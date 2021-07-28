be14030144d78b9421f36b3f10db3d92373f0cca
Robert Soriano <sorianorobertc@gmail.com>
2021-07-28 13:46:39 +0800

    update example base url

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/v-perfect-signature/' : './',
 })
