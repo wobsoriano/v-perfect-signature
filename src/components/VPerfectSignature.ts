d5b4a2e4575ed18b52287aaa3c604d21a7ca0dc7
wobsoriano <sorianorobertc@gmail.com>
2022-12-03 11:26:54 -0800

    fix: getStroke export

--- src/components/VPerfectSignature.ts
+++ src/components/VPerfectSignature.ts
@@ -1,7 +1,7 @@
 import type { PropType } from 'vue-demi'
 import { defineComponent } from 'vue-demi'
 import type { StrokeOptions } from 'perfect-freehand'
-import { getStroke } from 'perfect-freehand'
+import getStroke from 'perfect-freehand'
 
 import h from '../utils/h-demi'
 import getSvgPathFromStroke from '../utils/get-svg-path-from-stroke'
